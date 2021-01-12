b82ff26f9419a6d0cab94d025dd2bb3befb7a4f8
Romario Gustave <romagustave10@gmail.com>
2021-01-12 06:07:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+fetch("http://localhost:3000/dogs", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                    dogName: "Byron",
+                    dogBreed: "Poodle"
+                })
+                .then(res => {
+                    return res.json()
+                })
+
+                .then(s = data => console.log(data))
+                .catch(error => console.loglog('error'))
